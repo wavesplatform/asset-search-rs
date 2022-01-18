687b3b3e080c8518a7a211a77c6a0e914db7101a
Аleksej Petrov <apetrov@wavesplatform.com>
2022-01-18 15:37:03 +0300

    fixed images-service api client

--- src/lib/api_clients/images.rs
+++ src/lib/api_clients/images.rs
@@ -2,8 +2,7 @@ use async_trait::async_trait;
 use bytes::Bytes;
 use reqwest::StatusCode;
 use std::sync::Arc;
-use tokio::time::Instant;
-use wavesexchange_log::{debug, trace};
+use wavesexchange_log::trace;
 
 use super::{ApiBaseUrl, Error, HttpClient};
 
@@ -15,11 +14,9 @@ pub trait Client: ApiBaseUrl {
 #[async_trait]
 impl Client for HttpClient {
     async fn svg(&self, asset_id: &str) -> Result<Bytes, Error> {
-        let endpoint_url = format!("{}/{}.svg", &self.root_url, asset_id);
+        let endpoint_url = format!("{}{}.svg", &self.root_url, asset_id);
 
-        trace!("Images service request:\n\tURL: {}", endpoint_url);
-
-        let req_start_time = Instant::now();
+        trace!("Images service request:\n\tURL: {}", endpoint_url,);
 
         let resp = self.client.get(&endpoint_url).send().await.map_err(|err| {
             Error::HttpRequestError(
@@ -27,11 +24,6 @@ impl Client for HttpClient {
                 "Failed to the get result from the images service".to_string(),
             )
         })?;
-
-        debug!(
-            "Images service request has took {:?}ms",
-            req_start_time.elapsed().as_millis()
-        );
 
         if resp.status() == StatusCode::OK {
             resp.bytes()
