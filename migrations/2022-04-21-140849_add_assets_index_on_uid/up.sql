fe1201762a978ed8109f0cf614604efb998a8205
Аleksej Petrov <apetrov@wavesplatform.com>
2022-04-21 17:53:55 +0300

    added missing index

--- migrations/2022-04-21-140849_add_assets_index_on_uid/up.sql
+++ migrations/2022-04-21-140849_add_assets_index_on_uid/up.sql
@@ -1,1 +1,4 @@
 CREATE UNIQUE INDEX IF NOT EXISTS assets_uid_idx ON assets (uid);
+
+CREATE INDEX IF NOT EXISTS assets_id_uid_desc_block_uid_partial_idx
+    ON assets (id, uid DESC, block_uid) WHERE nft = false;
